56dd6fe91cf6823fa97fe5f7cc8df24f88100c3d
romainsessa <romainsessa@gmail.com>
2024-05-23 20:52:32 +0200

    P1C5 - Boucle et condition

--- src/ej/Main.java
+++ src/ej/Main.java
@@ -3,11 +3,14 @@ package ej;
 public class Main {
 
 	public static void main(String[] args) {
-		affichage("Marty");
-	}
-	
-	public static void affichage(String texte) {
-		System.out.println("Bonjour " + texte);
+		int[] tableau = {10, 100, 50, 22, 1080, 27000, 0};
+		int cpt = 0;
+		for(int i=0; i<7; i++) {
+			if(tableau[i] == 0) {
+				cpt++;
+			}
+		}
+		System.out.println(cpt);
 	}
 	
 }
