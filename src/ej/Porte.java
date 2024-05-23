78387f29231daaf61bd1b111c65ed65d726f4a0e
romainsessa <romainsessa@gmail.com>
2024-05-23 21:09:13 +0200

    P2C2 - Fix Porte

--- src/ej/Porte.java
+++ src/ej/Porte.java
@@ -4,7 +4,7 @@ public class Porte extends Bloc {
 
 	private boolean verrouillee;
 	
-	public Porte(final int longueur, final int largeur, final int hauteur, final boolean verrouille) {
+	public Porte(final int longueur, final int largeur, final int hauteur, final boolean verrouillee) {
 		super(longueur, largeur, hauteur);
 		this.verrouillee = verrouillee;
 	}
