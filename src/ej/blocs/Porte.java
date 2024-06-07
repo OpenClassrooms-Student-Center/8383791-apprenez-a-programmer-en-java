6294a1ee1cb69f3ffb2417da671bd62bdae3bd0e
romainsessa <romainsessa@gmail.com>
2024-06-07 15:26:14 +0200

    Logging

--- src/ej/blocs/Porte.java
+++ src/ej/blocs/Porte.java
@@ -1,6 +1,14 @@
-package ej;
+package ej.blocs;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import ej.IllegalBlocException;
+import ej.PorteVerrouilleException;
 
 public class Porte extends Bloc {
+	
+	private static Logger logger = LogManager.getLogger(Porte.class);
 
 	private boolean verrouillee;
 
@@ -17,6 +25,7 @@ public class Porte extends Bloc {
 
 	public void verrouiller() throws PorteVerrouilleException {
 		if (verrouillee) {
+			logger.error("La porte ne peut pas être verouillée car c'est déjà le cas.");
 			throw new PorteVerrouilleException();
 		} else {
 			verrouillee = true;
